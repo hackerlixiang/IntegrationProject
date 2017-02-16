45568788eecac11d04520926f4eaf407d49262aa
lixiang <565180713@qq.com>
2017-02-16 11:02:53 +0800

    加入中文转拼音方法

--- src/test/java/com/myproject/www/test/ChineseUtilsTest.java
+++ src/test/java/com/myproject/www/test/ChineseUtilsTest.java
@@ -11,7 +11,10 @@ public class ChineseUtilsTest {
 	public static void main(String[] args) {
 		System.out.println(ChineseUtils.isChinese('啊'));
 		System.out.println(ChineseUtils.getChineseByStr("asdasd阿萨德aa1111d的"));
+		
+		 String cnStr = "驀123aa!-";
+		 System.out.println("汉字串拼音："+ChineseUtils.cn2Spell(cnStr));
+		 System.out.println("汉字串首字母："+ChineseUtils.cn2FirstSpell(cnStr));
 	}
-	
 }
 
